0abaccd7cb673abed465d760a46d8cc7b21718e7
gulkovska <gylkovskasasha@icloud.com>
2025-05-02 20:31:36 +0300

    add docs for DeleteQueryBuilder

--- src/query-builder/builder/delete/DeleteQueryBuilder.ts
+++ src/query-builder/builder/delete/DeleteQueryBuilder.ts
@@ -1,16 +1,33 @@
 import {ClauseMixin} from "@/query-builder/builder/common/ClauseMixin";
 import {DeleteQuery} from "@/query-builder/queries/Delete";
 
-
+/**
+ * Builder class for constructing a SQL DELETE query.
+ * Extends {@link ClauseMixin} to support common SQL clauses (e.g., WHERE, JOIN).
+ */
 
 export class DeleteQueryBuilder extends ClauseMixin {
-    private tableName: string = "";
+    /**
+     * The name of the table from which rows will be deleted.
+     * @private
+     */
 
+    private tableName: string = "";
+    /**
+     * Sets the table name for the DELETE query.
+     *
+     * @param table - The name of the target table.
+     * @returns The current DeleteQueryBuilder instance.
+     */
     from(table: string): this {
         this.tableName = table;
         return this;
     }
-
+    /**
+     * Builds and returns the final DELETE query object.
+     *
+     * @returns A {@link DeleteQuery} object representing the DELETE SQL statement.
+     */
     build(): DeleteQuery {
         return {
             type: "DELETE",
