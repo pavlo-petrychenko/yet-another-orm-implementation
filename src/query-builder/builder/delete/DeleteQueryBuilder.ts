1be4f432b6ce08ef5a3441586c2254c761bc4420
Valerie <valeriachelushkina@gmail.com>
2025-05-05 21:45:37 +0300

    add logging for error handling in DeleteQueryBuilder

--- src/query-builder/builder/delete/DeleteQueryBuilder.ts
+++ src/query-builder/builder/delete/DeleteQueryBuilder.ts
@@ -1,21 +1,42 @@
-import {ClauseMixin} from "@/query-builder/builder/common/ClauseMixin";
-import {DeleteQuery} from "@/query-builder/queries/Delete";
-
-
+import { ClauseMixin } from "@/query-builder/builder/common/ClauseMixin";
+import { DeleteQuery } from "@/query-builder/queries/Delete";
+import pino from "pino";
 
 export class DeleteQueryBuilder extends ClauseMixin {
-    private tableName: string = "";
+  private logger = pino({
+    transport: {
+      target: "pino-pretty",
+      options: { colorize: true },
+    },
+  });
 
-    from(table: string): this {
-        this.tableName = table;
-        return this;
-    }
+  private tableName: string = "";
 
-    build(): DeleteQuery {
-        return {
-            type: "DELETE",
-            table: this.tableName,
-            ...this.buildCommonClauses()
-        };
+  from(table: string): this {
+    // Validate table name
+    if (!table || typeof table !== "string") {
+      this.logger.error({ table }, "DeleteQueryBuilder: Invalid table name");
+      throw new Error("Table name must be a non-empty string");
     }
+    this.tableName = table;
+    this.logger.debug({ table }, "DeleteQueryBuilder: Set table name");
+    return this;
+  }
+
+  build(): DeleteQuery {
+    this.logger.debug(
+      {
+        type: "DELETE",
+        table: this.tableName,
+        ...this.buildCommonClauses(),
+      },
+      "Built DELETE query"
+    );
+
+    return {
+      type: "DELETE",
+      table: this.tableName,
+      ...this.buildCommonClauses(),
+    };
+  }
 }
